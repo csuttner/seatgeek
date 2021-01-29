f3000930972ba8bbde57b64aafac0a3372058716
Clay Suttner <csuttner@gmail.com>
2021-01-28 17:27:05 -0800

    Initial Commit

---
+++ SeatGeek/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeatGeek
+//
+//  Created by Clay Suttner on 1/28/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
