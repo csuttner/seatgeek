7ea147e3c0f8b5fb5c4f6700f057d004e1ab7cf8
Clay Suttner <csuttner@gmail.com>
2021-02-01 09:21:21 -0800

    adds Authorization.swift

---
+++ SeatGeek/Helpers/Authorization.swift
@@ -0,0 +1,13 @@
+//
+//  Secrets.swift
+//  SeatGeek
+//
+//  Created by Clay Suttner on 1/28/21.
+//
+
+import Foundation
+
+struct Authorization {
+    static let clientId = "CLIENT_ID"
+    static let clientSecret = "CLIENT_SECRET"
+}
